4e5f78c2b88a8662163a23ac81a801ede6e11e73
jayant2472 <jayantm42@gmail.com>
2019-11-14 17:38:55 +0530

    flutter file picker.

---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_file_picker/src/file_picker_demo.dart';
+
+void main() => runApp(new FilePickerDemo());
